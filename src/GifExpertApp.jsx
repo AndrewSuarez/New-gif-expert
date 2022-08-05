7025ca25ea97fd4bc1877b0cddb7d5304106616d
Andres Suarez <andresesuareze@gmail.com>
2022-08-04 23:06:54 -0400

    First Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import { AddCategory, GifGrid } from "./components"
+
+export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['Mushoku'])
+
+  const handleAddCategory = (category) => {
+    if(categories.includes(category)) return;
+    setCategories([category, ...categories])
+  }
+
+  return (
+    <>
+      {/* titulo */}
+      <h1>GifExpertApp</h1>
+
+      {/* input */}
+      <AddCategory 
+        onNewCategory={(category) => handleAddCategory(category)} 
+      />
+
+      {/* Listado de Gifs */}
+        {categories.map(category => ( 
+            <GifGrid key={category} category={category}/>
+        ))}
+    </>
+  )
+}
